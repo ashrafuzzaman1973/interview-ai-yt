1cd7d75ddb17c37312ce5736c307e70d224d85a6
Ashrafuzzaman Ashraf <ashrafdiu1973@gmail.com>
2026-08-10 17:40:38 +0600

    Add CORS middleware for specific origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
     origin: "https://interview-ai-yt-new.vercel.app",
     credentials: true
@@ -15,11 +16,8 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
